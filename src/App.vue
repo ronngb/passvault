cc224fe467eba4ff3ad7dda5bb9597d14757526c
ronngb <ronny.gabi@gmail.com>
2021-03-15 11:23:55 +0800

    Change the components name inside the app.vue base on vuejs Multi-Word component & PascalCase for single-file component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-    <Navbar></Navbar>
+    <SearchBar />
     <div class="container-fluid">
       <div class="row">
-        <Acctlist></Acctlist>
-        <Acctinfo></Acctinfo>
+        <AccountList />
+        <AccountInfo />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-import Acctlist from "./components/Acctlist.vue";
-import Acctinfo from "./components/Acctinfo.vue";
+import SearchBar from "./components/SearchBar.vue";
+import AccountList from "./components/AccountList.vue";
+import AccountInfo from "./components/AccountInfo.vue";
 
 export default {
   name: "App",
   components: {
-    Navbar,
-    Acctlist,
-    Acctinfo,
+    SearchBar,
+    AccountList,
+    AccountInfo,
   },
 };
 </script>
