c4b70922b08da7a28d867bb4fec7327741a0cb4d
ronngb <ronny.gabi@gmail.com>
2021-11-23 10:52:00 +0800

    refactor: reorganize the main.js

---
+++ src/fontawesome.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faGlobe,
+  faPencilAlt,
+  faTrash,
+  faEye,
+  faCopy,
+  faEyeSlash,
+  faExclamationCircle,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faGlobe,
+  faPencilAlt,
+  faTrash,
+  faEye,
+  faCopy,
+  faEyeSlash,
+  faExclamationCircle
+);
+
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -1,33 +1,12 @@
 import Vue from 'vue';
 import App from './app/App.vue';
 import VueRouter from 'vue-router';
+import FontAwesomeIcon from './fontawesome.js';
 import { routes } from './app/routes/routes';
 import { store } from './app/store/store';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faGlobe,
-  faPencilAlt,
-  faTrash,
-  faEye,
-  faCopy,
-  faEyeSlash,
-  faExclamationCircle,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(
-  faGlobe,
-  faPencilAlt,
-  faTrash,
-  faEye,
-  faCopy,
-  faEyeSlash,
-  faExclamationCircle
-);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
