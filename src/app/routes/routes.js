a4b6c3fa9fe005ec0bdcb4bf2a80877033695d1e
ronngb <ronny.gabi@gmail.com>
2021-12-23 15:52:06 +0800

    refactor: use props, append 'detail' & 'edit' nested routes

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,8 +1,17 @@
-import Acct from '../components/account/AccountList.vue';
-import DetailsForm from '../components/information/DetailsForm.vue';
-import Information from '../components/information/Information.vue';
+import AddForm from '../components/information/AddForm.vue'
+import Account from '../views/Account.vue'
+import AccountDetail from '../views/AccountDetail.vue'
+import AccountEdit from '../views/AccountEdit.vue'
 
 export const routes = [
-	// { path: '/', component: DetailsForm },
-	{ path: '/account/:id', component: DetailsForm },
-];
+  { path: '/account/add', name: 'addForm', component: AddForm },
+  {
+    path: '/account/:id',
+    component: Account,
+    props: true,
+    children: [
+      { path: '', name: 'detail', component: AccountDetail, props: true },
+      { path: 'edit', name: 'edit', component: AccountEdit, props: true },
+    ],
+  },
+]
