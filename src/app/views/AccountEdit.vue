d8b2c87837961319909a6e1c295a1e90d418dc60
ronngb <ronny.gabi@gmail.com>
2021-12-23 15:59:53 +0800

    refactor: account(), append mapGetters

--- src/app/views/AccountEdit.vue
+++ src/app/views/AccountEdit.vue
@@ -1,82 +1,85 @@
 <template>
-	<form class="acct-info__form">
-		<!-- Username Start -->
-		<div class="form-row">
-			<div class="form-group col-8 col-md-5">
-				<label>Username</label>
-				<input
-					id="username"
-					type="text"
-					class="form-control"
-					v-model="formData.username" />
-			</div>
-		</div>
-		<!-- Password Start -->
-		<div class="form-row">
-			<div class="form-group col-8 col-md-5">
-				<label>Password</label>
-				<input
-					id="password"
-					type="password"
-					:class="{ 'is-invalid': hasError, 'form-control': true }"
-					v-model="formData.password" />
-				<div v-if="hasError" class="invalid-feedback">
-					Your password is required.
-				</div>
-			</div>
-		</div>
-		<!-- EDIT BUTTONS -->
-		<div class="acct-edit_buttons">
-			<button
-				@click="updateAccount(account.id)"
-				type="button"
-				class="btn btn-default btn-light mr-1">
-				Save
-			</button>
-			<button
-				@click="$router.go(-1)"
-				type="button"
-				class="btn btn-default btn-light ml-1">
-				Cancel
-			</button>
-		</div>
-	</form>
+  <form class="acct-info__form">
+    <!-- Username Start -->
+    <div class="form-row">
+      <div class="form-group col-8 col-md-5">
+        <label>Username</label>
+        <input
+          id="username"
+          type="text"
+          class="form-control"
+          v-model="formData.username" />
+      </div>
+    </div>
+    <!-- Password Start -->
+    <div class="form-row">
+      <div class="form-group col-8 col-md-5">
+        <label>Password</label>
+        <input
+          id="password"
+          type="password"
+          :class="{ 'is-invalid': hasError, 'form-control': true }"
+          v-model="formData.password" />
+        <div v-if="hasError" class="invalid-feedback">
+          Your password is required.
+        </div>
+      </div>
+    </div>
+    <!-- EDIT BUTTONS -->
+    <div class="acct-edit_buttons">
+      <button
+        @click="updateAccount(account.id)"
+        type="button"
+        class="btn btn-default btn-light mr-1">
+        Save
+      </button>
+      <button
+        @click="$router.go(-1)"
+        type="button"
+        class="btn btn-default btn-light ml-1">
+        Cancel
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-	data() {
-		return {
-			hasError: false,
-			formData: { username: '', password: '' },
-		};
-	},
-	mounted() {
-		this.initFormData();
-	},
-	computed: {
-		account() {
-			return this.$store.getters.getAccount(this.$route.params.id);
-		},
-	},
-	methods: {
-		initFormData() {
-			for (let prop in this.formData)
-				this.formData[prop] = this.account[prop];
-		},
-		updateAccount(id) {
-			if (this.formData.password == '') {
-				this.hasError = true;
-			} else {
-				const accountObj = {
-					...this.formData,
-					accountId: id,
-				};
-				this.$store.dispatch('updateAccount', accountObj);
-			}
-		},
-	},
-};
+  props: ['id'],
+  data() {
+    return {
+      hasError: false,
+      formData: { username: '', password: '' },
+    }
+  },
+  mounted() {
+    this.initFormData()
+  },
+  computed: {
+    ...mapGetters(['getAccount']),
+    account() {
+      return this.getAccount(this.id)
+    },
+  },
+  methods: {
+    initFormData() {
+      for (let prop in this.formData) this.formData[prop] = this.account[prop]
+    },
+    updateAccount(id) {
+      if (this.formData.password == '') {
+        this.hasError = true
+      } else {
+        const accountObj = {
+          ...this.formData,
+          accountId: id,
+        }
+        this.$store.dispatch('updateAccount', accountObj)
+      }
+    },
+  },
+}
 </script>
 
 <style></style>
