1810d20e38fd5691dbc330881a9ed32b92c4ade9
ronngb <ronny.gabi@gmail.com>
2022-01-14 17:36:44 +0800

    refactor: rename the name property value to more precise name

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,17 +1,28 @@
-import AddForm from '../components/information/AddForm.vue'
 import Account from '../views/Account.vue'
+import AccountCreate from '../views/AccountCreate.vue'
 import AccountDetail from '../views/AccountDetail.vue'
 import AccountEdit from '../views/AccountEdit.vue'
 
+// TODO: rename all name property i.e create -> account-create
 export const routes = [
-  { path: '/account/add', name: 'addForm', component: AddForm },
+  { path: '/account/create', name: 'account-create', component: AccountCreate },
   {
     path: '/account/:id',
     component: Account,
     props: true,
     children: [
-      { path: '', name: 'detail', component: AccountDetail, props: true },
-      { path: 'edit', name: 'edit', component: AccountEdit, props: true },
+      {
+        path: '',
+        name: 'account-detail',
+        component: AccountDetail,
+        props: true,
+      },
+      {
+        path: 'edit',
+        name: 'account-edit',
+        component: AccountEdit,
+        props: true,
+      },
     ],
   },
 ]
