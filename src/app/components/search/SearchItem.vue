9452802504780ddce6f67afd2f956fda3456a631
ronngb <ronny.gabi@gmail.com>
2022-07-16 01:20:18 +0800

    refactor: Convert to SCSS

--- src/app/components/search/SearchItem.vue
+++ src/app/components/search/SearchItem.vue
@@ -1,22 +1,26 @@
 <template>
-  <form action="">
+  <form>
     <label for="Search">Search</label>
     <input
-      @keyup="SEARCH_ACCOUNT(searchItem)"
+      type="search"
       class="search-input"
       v-model="searchItem"
       @focus="setFocus"
       @blur="setFocus"
-      type="search" />
-    <font-awesome-icon :icon="['fas', 'search']" class="search-icon" />
+      @keyup="SEARCH_ACCOUNT(searchItem)" />
+    <BaseIcon :icon="'search'" />
   </form>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
+import BaseIcon from '../base/BaseIcon.vue'
 
 export default {
   name: 'SearchItem',
+  components: {
+    BaseIcon,
+  },
   data() {
     return {
       searchItem: '',
@@ -27,6 +31,7 @@ export default {
   methods: {
     ...mapMutations(['SEARCH_ACCOUNT']),
     setFocus(event) {
+      // REFACTOR: maybe the target api is redundant
       event.target.previousSibling.classList.toggle('active')
       event.target.nextSibling.classList.toggle('active')
       if (event.type == 'blur') {
@@ -40,41 +45,35 @@ export default {
 <style lang="scss" scoped>
 form {
   position: relative;
-  color: $color-light-grey;
 }
 
 label {
-  position: absolute;
   top: 50%;
   left: 5%;
   margin-top: -10.5px;
   transform: translateX(0);
-  transition: all 250ms ease-in-out 200ms;
+  transition: all 0.25s ease-in-out 0.2s;
+
+  &.active {
+    opacity: 0;
+    transform: translateX(-25px);
+  }
 }
 
-label.active {
-  transform: translateX(-25px);
-  opacity: 0;
+svg {
+  transition: transform 0.25s ease-in-out;
+
+  &.active {
+    transform: rotate(90deg);
+  }
 }
 
 .search-input {
   height: 50px;
-  font-weight: 600;
   border-radius: 50px;
+  // TODO: make the rgba smaller
   box-shadow: 4px 4px 6px rgba(187, 195, 206, 0.6),
     -5px -5px 7px rgba(255, 255, 255, 0.7);
-}
-
-.search-icon {
-  position: absolute;
-  left: 2%;
-  top: 50%;
-  margin-top: -8px;
-  color: #90a4ae;
-  transition: transform 250ms ease-in-out;
-}
-
-.search-icon.active {
-  transform: rotate(90deg);
+  // font-weight: 600;
 }
 </style>
