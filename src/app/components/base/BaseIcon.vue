576abbab2831e3ef3a6fed62d785276b32048b34
ronngb <ronny.gabi@gmail.com>
2022-07-16 01:17:02 +0800

    feat: To simplify adding new icon

---
+++ src/app/components/base/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <font-awesome-icon :icon="['fas', icon]" />
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+svg {
+  position: absolute;
+  left: 2%;
+  top: 50%;
+  margin-top: -8px;
+  color: $color-light-grey;
+  font-size: 1.6rem;
+}
+</style>
