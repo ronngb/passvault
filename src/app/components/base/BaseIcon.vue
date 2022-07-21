0609a5e76f5ec68d22935a5a65900cf55e02ef08
ronngb <ronny.gabi@gmail.com>
2022-07-21 12:06:13 +0800

    refactor: Make it efficient

--- src/app/components/base/BaseIcon.vue
+++ src/app/components/base/BaseIcon.vue
@@ -1,5 +1,7 @@
 <template>
-  <font-awesome-icon :icon="['fas', icon]" />
+  <font-awesome-icon
+    :icon="['fas', icon]"
+    :class="[indefault ? 'default-style' : '']" />
 </template>
 
 <script>
@@ -9,17 +11,24 @@ export default {
       type: String,
       default: '',
     },
+    indefault: {
+      type: Boolean,
+      default: true,
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 svg {
+  color: $color-light-grey;
+  font-size: 1.6rem;
+}
+
+.default-style {
   position: absolute;
   left: 2%;
   top: 50%;
   margin-top: -8px;
-  color: $color-light-grey;
-  font-size: 1.6rem;
 }
 </style>
