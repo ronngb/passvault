a8349fb1e865a741276918316dfcddea774f7d5d
ronngb <ronny.gabi@gmail.com>
2022-12-03 17:09:37 +0800

    style: append mixins for neumorp button

--- src/app/components/neumorp/NeumorpButton.vue
+++ src/app/components/neumorp/NeumorpButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-click="() => $listeners.click.fns()" class="btn">
+  <button v-click="() => $listeners.click.fns()" :class="classObj">
     <slot></slot>
   </button>
 </template>
@@ -15,6 +15,14 @@ export default {
       },
     },
   },
+  computed: {
+    classObj() {
+      return {
+        btn: true,
+        edit: this.$route.name == 'account-edit',
+      }
+    },
+  },
 }
 </script>
 
@@ -26,12 +34,26 @@ export default {
       outline: none;
       text-shadow: $tertiary-color 1px 0px 15px;
     }
+    &:hover > svg {
+      color: inherit;
+      transition: color 180ms ease-in-out 100ms;
+    }
   }
   &-danger {
-    color: $danger;
+    color: $danger-color;
     &:focus {
       outline: none;
-      text-shadow: $danger 1px 0px 15px;
+      text-shadow: $danger-color 1px 0px 15px;
+    }
+    &:hover > svg {
+      color: inherit;
+      transition: color 180ms ease-in-out 100ms;
+    }
+  }
+  &-info {
+    &:hover > svg {
+      color: $info-color;
+      transition: color 180ms ease-in-out 100ms;
     }
   }
 
@@ -41,15 +63,14 @@ export default {
     border-radius: 10px;
     margin: 0 10px;
     font-weight: bold;
-    box-shadow: neumorp-button-initial($contained);
-
+    @include neumorp-button-initial(contained);
     &:hover {
       opacity: 0.5;
       transition: opacity 200ms ease-out;
     }
     &:active {
       opacity: 1;
-      box-shadow: neumorp-button-press($contained);
+      @include neumorp-button-active(contained);
     }
   }
 
@@ -60,14 +81,36 @@ export default {
     bottom: 30px;
     left: 20vw;
     position: fixed;
-    box-shadow: neumorp-button-initial($float);
+
+    @include neumorp-button-initial(float);
     &:hover {
       opacity: 0.5;
       transition: opacity 200ms ease-out;
     }
     &:active {
       opacity: 1;
-      box-shadow: neumorp-button-press($float);
+      @include neumorp-button-active(float);
+    }
+  }
+
+  &-mini {
+    width: 36px;
+    height: 36px;
+    margin: 0 10px;
+    border-radius: 32%;
+    position: relative;
+    border: 1px solid $primary-bg-color;
+    @include neumorp-button-initial(mini);
+    transition: box-shadow 200ms ease, transform 200ms ease;
+    &:active {
+      @include neumorp-button-active(mini);
+      transform: translateY(1px);
+    }
+
+    &.edit.btn-submit,
+    &.edit.btn-info {
+      transform: scale(0);
+      transition: transform 200ms ease-in-out 500ms;
     }
   }
 }
