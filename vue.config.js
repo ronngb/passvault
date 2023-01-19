14f0cc65188310c69f782e3b86d8bd3e2c941972
ronngb <ronny.gabi@gmail.com>
2023-01-19 16:30:41 +0800

    style: replace the favicon & website name

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'PassVault',
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+  },
   css: {
-    sourceMap: true,
+    sourceMap: process.env.NODE_ENV === 'production' ? false : true,
     loaderOptions: {
       scss: {
         additionalData: `
-        @import "~@/app/assets/scss/_variables.scss";
-        @import "~@/app/assets/scss/_functions.scss";
-        @import "~@/app/assets/scss/_mixins.scss";
+        @import "~@/app/assets/scss/abstracts/_variables.scss";
+        @import "~@/app/assets/scss/abstracts/_functions.scss";
+        @import "~@/app/assets/scss/abstracts/_mixins.scss";
         `,
       },
     },
